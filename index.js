60d76bae7c68726ebe256d93d5a1a3931d66a6df
Jennifer Hu <jhu@LDI.INT>
2022-11-14 20:23:59 -0500

    add a purple, double click to white, clear button added

--- index.js
+++ index.js
@@ -12,9 +12,31 @@ painting.addEventListener('click', (e)=>{
     e.target.style.backgroundColor = selectedColor;
 })
 
+painting.addEventListener('dblclick', (e)=>{
+    e.target.style.backgroundColor = "white";
+})
 
 for (let i = 0; i < colorSelectors.length; i++){
     colorSelectors[i].addEventListener('click', (e)=>{
         selectedColor = e.target.id;
-    })
+    });
 }
+
+const palette = document.querySelector('.palette');
+
+const purple = document.createElement('div');
+purple.setAttribute('class','color-choice');
+purple.setAttribute('id','purple');
+purple.textContent = "■";
+palette.appendChild(purple);
+
+const clearButton = document.createElement('button');
+clearButton.textContent = "Clear";
+palette.appendChild(clearButton);
+
+clearButton.addEventListener('click', ()=>{
+    const pixels = document.querySelectorAll(".pixel");
+    for (let i = 0; i < pixels.length; i++) {
+        pixels[i].style.backgroundColor = "white";
+    }
+})
